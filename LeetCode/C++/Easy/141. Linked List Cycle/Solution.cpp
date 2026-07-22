6de4bdfb1153a4d9f6c2967880b9a93e4587a5c2
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-07-22 11:00:56 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/141. Linked List Cycle/Solution.cpp
+++ LeetCode/C++/Easy/141. Linked List Cycle/Solution.cpp
@@ -14,7 +14,7 @@ public:
 
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
-            fast=fast->next->n;
+            fast=fast->next->next;
 
             if(slow==fast){
                 return true;
