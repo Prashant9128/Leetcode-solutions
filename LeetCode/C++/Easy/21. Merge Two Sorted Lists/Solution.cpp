1c84c7316d88a72506cb02a5174d885cf8fd471f
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-07-22 10:19:38 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/21. Merge Two Sorted Lists/Solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 !=NULL && list2==NULL) return list1;
+        if(list2 !=NULL && list1==NULL) return list2;
+
+        vector<int>arr;
+        while(list1!=nullptr){
+            arr.push_back(list1->val);
+            list1=list1->next;
+        }
+        while(list2!=nullptr){
+            arr.push_back(list2->val);
+            list2=list2->next;
+        }
+        sort(arr.begin(),arr.end());
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr=dummy;
+
+        for(int i=0;i<arr.size();i++){
+            curr->next=new ListNode(arr[i]);
+            curr=curr->next;
+        }
+        return dummy->next;
+        
+    }
+};
