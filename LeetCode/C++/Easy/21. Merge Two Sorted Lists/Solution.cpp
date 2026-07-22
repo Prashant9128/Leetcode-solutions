491e9026d67b4cbf13892a8ed20d7a98e7ac339d
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-07-22 10:32:59 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/21. Merge Two Sorted Lists/Solution.cpp
+++ LeetCode/C++/Easy/21. Merge Two Sorted Lists/Solution.cpp
@@ -8,31 +8,50 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1 !=NULL && list2==NULL) return list1;
+//         if(list2 !=NULL && list1==NULL) return list2;
+
+//         vector<int>arr;
+//         while(list1!=nullptr){
+//             arr.push_back(list1->val);
+//             list1=list1->next;
+//         }
+//         while(list2!=nullptr){
+//             arr.push_back(list2->val);
+//             list2=list2->next;
+//         }
+//         sort(arr.begin(),arr.end());
+
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* curr=dummy;
+
+//         for(int i=0;i<arr.size();i++){
+//             curr->next=new ListNode(arr[i]);
+//             curr=curr->next;
+//         }
+//         return dummy->next;
+        
+//     }
+// };
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 !=NULL && list2==NULL) return list1;
-        if(list2 !=NULL && list1==NULL) return list2;
-
-        vector<int>arr;
-        while(list1!=nullptr){
-            arr.push_back(list1->val);
-            list1=list1->next;
+        if(list1==NULL){
+            return list2;
         }
-        while(list2!=nullptr){
-            arr.push_back(list2->val);
-            list2=list2->next;
+        if(list2==NULL){
+            return list1;
         }
-        sort(arr.begin(),arr.end());
-
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr=dummy;
-
-        for(int i=0;i<arr.size();i++){
-            curr->next=new ListNode(arr[i]);
-            curr=curr->next;
+        if(list1->val<=list2->val){
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }else{
+            list2->next=mergeTwoLists(list1,list2->next);;
+            return list2;
         }
-        return dummy->next;
-        
     }
 };
