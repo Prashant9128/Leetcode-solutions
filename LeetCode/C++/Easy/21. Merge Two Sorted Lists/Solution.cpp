6b464a411efacb894eb8cca7872288f4c123da0d
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-07-22 10:33:18 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/21. Merge Two Sorted Lists/Solution.cpp
+++ LeetCode/C++/Easy/21. Merge Two Sorted Lists/Solution.cpp
@@ -50,7 +50,7 @@ public:
             list1->next=mergeTwoLists(list1->next,list2);
             return list1;
         }else{
-            list2->next=mergeTwoLists(list1,list2->next);;
+            list2->next=mergeTwoLists(list1,list2->nexty);
             return list2;
         }
     }
