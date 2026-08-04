6e61973d97b724842ba2868732f2dd237ceb8a0c
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-08-04 10:28:06 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/70. Climbing Stairs/Solution.cpp
+++ LeetCode/C++/Easy/70. Climbing Stairs/Solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2)
-            return n;
-
-        int prev2 = 1;
-        int prev1 = 2;
-
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
-        return prev1;
+        return dp[n];
     }
 };
