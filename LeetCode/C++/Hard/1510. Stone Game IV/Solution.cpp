27198d3edc8a5c6f3caa2f8379b31bd6fd46a18a
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-08-10 20:02:33 +0530

    Add Hard C++ Solution from LeetCode

--- LeetCode/C++/Hard/1510. Stone Game IV/Solution.cpp
+++ LeetCode/C++/Hard/1510. Stone Game IV/Solution.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        
+        vector<bool> dp(n + 1, false);
+
+        // dp[0] = false
+        // 0 stones -> current player cannot move -> loses
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                int square = j * j;
+
+                // If removing square stones
+                // makes opponent reach a losing state
+                if (dp[i - square] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
