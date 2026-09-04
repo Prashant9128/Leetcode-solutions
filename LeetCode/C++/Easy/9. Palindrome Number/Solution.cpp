6d8471f4387549bffa8942c60cd7679960dd2555
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-09-04 11:01:47 +0530

    Add Easy C++ Solution for 9. Palindrome Number from LeetCode

--- LeetCode/C++/Easy/9. Palindrome Number/Solution.cpp
+++ LeetCode/C++/Easy/9. Palindrome Number/Solution.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) 
+        if (x >= 0) {
+
+            int num = x;
+            int rev = 0;
+            while (num != 0) {
+                int digit = num % 10;
+                rev = rev * 10 + digit;
+                num = num / 10;
+            }
+            return rev == x;
+
+        } else {
             return false;
-
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
         }
-
-        return (x == reversedHalf || x == reversedHalf / 10);
     }
 };
