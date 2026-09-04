ede9ee23e15882c5b0557ace4968ed6953d5bc16
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-09-04 11:37:03 +0530

    Add Medium C++ Solution for 7. Reverse Integer from LeetCode

--- LeetCode/C++/Medium/7. Reverse Integer/README.md
+++ LeetCode/C++/Medium/7. Reverse Integer/README.md
@@ -8,8 +8,8 @@
 Math
 
 ### 🚀 Performance
-- **Runtime:** 5 ms
-- **Memory:** 8.4 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Math/7. Reverse Integer/Solution.cpp
+++ LeetCode/Patterns/Math/7. Reverse Integer/Solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        int rev=0;
-        while(x!=0){
-            int digit=x%10;
-            x/=10;
-            if(rev>INT_MAX/10||rev==INT_MAX){
-                return 0;
-            }
-            if(rev<INT_MIN/10||rev==INT_MIN){
-                return 0;
-            }
+        int ans = 0;
 
-            rev=rev*10+digit;
+        while(x != 0){
+            int digit = x % 10;
+            x=x/10;
+
+            if(ans > INT_MAX / 10 || ans < INT_MIN / 10)
+            return 0;
+
+            ans = ans * 10 + digit;
         }
-        return rev;
-        
+        return ans;
     }
 };
