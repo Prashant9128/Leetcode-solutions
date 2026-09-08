0ecf4b96d9cdc9ef4324f608df5ddd03e846c4dd
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-09-09 01:11:12 +0530

    Add Easy C++ Solution for 66. Plus One from LeetCode

--- LeetCode/C++/Easy/66. Plus One/Solution.cpp
+++ LeetCode/C++/Easy/66. Plus One/Solution.cpp
@@ -1,6 +1,8 @@
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
+ #include <vector>
+
+ class Solution {
+    public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
         int n = digits.size();
         for (int i = n - 1; i >= 0; --i) {
             if (digits[i] < 9) {
@@ -8,8 +10,8 @@ public:
                 return digits;
             }
             digits[i] = 0;
-        }
-        digits.insert(digits.begin(), 1);
-        return digits;
+            }
+            digits.insert(digits.begin(), 1);
+            return digits;
     }
-};
+    };
