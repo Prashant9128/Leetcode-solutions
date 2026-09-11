91c8db751a3baf2fb8d3899fed5bfb1e27eba785
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-09-11 10:26:12 +0530

    Add Medium C++ Solution for 189. Rotate Array from LeetCode

--- LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
@@ -3,13 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        while(k--){
-            int last=nums[n-1];
-            for(int i=n-1;i>0;i--){
-                nums[i]=nums[i-1];
-            }
-            nums[0]=last;
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
-
 };
