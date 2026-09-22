a81a6c408926e66298c5b675fd6b34cd603d40e9
Prashant Kumar Sharma <149091015+Prashant9128@users.noreply.github.com>
2026-09-22 09:35:09 +0530

    Add Easy C++ Solution for 242. Valid Anagram from LeetCode

--- LeetCode/C++/Easy/242. Valid Anagram/README.md
+++ LeetCode/C++/Easy/242. Valid Anagram/README.md
@@ -8,8 +8,8 @@
 Hash Table, String, Sorting
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 9.6 MB
+- **Runtime:** 4 ms
+- **Memory:** 9.7 MB
 
 ---
 
--- LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
+++ LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
+        if(s.length()!=t.length()){
             return false;
-
-        int freq[26] = {0};
-
-        for (char ch : s) {
-            freq[ch - 'a']++;
         }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return t==s;
 
-        for (char ch : t) {
-            freq[ch - 'a']--;
-
-            if (freq[ch - 'a'] < 0)
-                return false;
-        }
-
-        return true;
+        
     }
 };
